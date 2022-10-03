efe165bf8e671e66d54ce46dc40d8910cfa72cb4
Cassius Cives Muñoz <cass-cives@hotmail.com>
2022-10-03 20:17:49 -0300

    Agregado de estilos y modal al hacer click

--- script.js
+++ script.js
@@ -5,28 +5,75 @@ const emailColumn = document.getElementById("email");
 const phoneColumn = document.getElementById("phone");
 const websiteColumn = document.getElementById("website");
 
-
 function pedidoApi(API_URL) {
     fetch(API_URL)
         .then((response) => response.json())
         .then((data) => recorrerData(data));
 }
 
-function crearElementos(column, data) {
+function crearElementos(tipo, column, data, id) {
     const h5 = document.createElement('h5');
     const h5Text = document.createTextNode(data);
     h5.appendChild(h5Text);
-    h5.classList += "dato";
+    //h5.classList += "dato mui--text-subhead";
+    h5.setAttribute("class", `dato mui--text-subhead`);
+    h5.setAttribute("id", `${tipo}${id}`);
+    h5.addEventListener("click", () => { activateModal(id); });
     column.appendChild(h5);
 }
 
 function recorrerData(data) {
     data.forEach(objeto => {
-        crearElementos(nameColumn, objeto.name)
-        crearElementos(usernameColumn, objeto.username)
-        crearElementos(emailColumn, objeto.email)
-        crearElementos(phoneColumn, objeto.phone)
-        crearElementos(websiteColumn, objeto.website)
+        crearElementos("name", nameColumn, objeto.name, objeto.id)
+        crearElementos("username", usernameColumn, objeto.username, objeto.id)
+        crearElementos("email", emailColumn, objeto.email, objeto.id)
+        crearElementos("phone", phoneColumn, objeto.phone, objeto.id)
+        crearElementos("website", websiteColumn, objeto.website, objeto.id)
     });
 }
+
+function activateModal(id) {
+    // initialize modal element
+    var modalEl = document.createElement('div');
+    modalEl.setAttribute("class", "modalEl");
+    const imageAndName = document.createElement('div');
+    imageAndName.setAttribute("class", "imageAndName");
+
+    const img = document.createElement('img');
+    img.src = `./img/image${id}.jpg`;
+    img.setAttribute("class", "imgModal");
+    imageAndName.appendChild(img);
+    const name = document.createElement('h2');
+    name.setAttribute("class", "mui--text-headline tituloModal");
+    const nameText = document.createTextNode(document.getElementById(`name${id}`).innerText);
+    name.appendChild(nameText);
+    imageAndName.appendChild(name);
+    modalEl.appendChild(imageAndName);
+
+    const otherData = document.createElement('div');
+    otherData.setAttribute("class", "otherData");
+    const phone = document.createElement('h3');
+    phone.setAttribute("class", "mui--text-subhead");
+    const phoneText = document.createTextNode(document.getElementById(`phone${id}`).innerText);
+    phone.appendChild(phoneText);
+    otherData.appendChild(phone);
+
+    const email = document.createElement('h3');
+    email.setAttribute("class", "mui--text-subhead");
+    const emailText = document.createTextNode(document.getElementById(`email${id}`).innerText);
+    email.appendChild(emailText);
+    otherData.appendChild(email);
+
+    const website = document.createElement('h3');
+    website.setAttribute("class", "mui--text-subhead");
+    const websiteText = document.createTextNode(document.getElementById(`website${id}`).innerText);
+    website.appendChild(websiteText);
+    otherData.appendChild(website);
+
+
+    modalEl.appendChild(otherData);
+    // show modal
+    mui.overlay('on', modalEl);
+}
+
 pedidoApi(API_URL);
